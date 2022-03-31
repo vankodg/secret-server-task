a3e44ab2c4530ff4cee77631ee4aeaf7e9663daf
vankodg <vankodaniel97@gmail.com>
2022-03-31 14:10:22 +0200

    prepare repo for heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import secrets from "./api/secrets.route.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
@@ -8,6 +10,14 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/v1/secret", secrets);
-app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+//app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 export default app;
